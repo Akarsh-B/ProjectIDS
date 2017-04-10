243bb746c24f4a0f3e5ef284b900d7dbb203827a
akarsh <akarshbalu@gmail.com>
2017-04-10 20:04:06 +0530

    Add files via upload

--- new.py
+++ new.py
@@ -1,14 +1,29 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, Response
 from serial import Serial
+from camera_pi import Camera
 
 app = Flask(__name__)
-#ser=Serial(/dev/ttyUSB0,9600)
+ser=Serial(/dev/ttyUSB0,9600)
 
 @app.route('/')
 def home():
 	return render_template("home.html")
 
 @app.route("/navigate/<direction>")
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 def navigation(direction):
 	if (direction == "forward"):
 		ser.write(b'150,0,150,0')
