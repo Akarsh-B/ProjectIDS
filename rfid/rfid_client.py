507255f672d0a5956aa3212805fdf73f44285aaa
Akarshbalu <akarshbalu@gmail.com>
2017-05-14 09:05:20 +0530

    Added correct RFID codes

---
+++ rfid/rfid_client.py
@@ -0,0 +1,18 @@
+import socket
+from serial import Serial
+
+ser = Serial("/dev/ttyACM0", 9600)
+
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+host = '192.168.0.107'  ///Need to change this everytime
+port = 12002
+
+client_socket.connect((host, port))
+
+
+while True:
+	uid_tag = ser.readline().strip().decode("utf-8")                 
+	client_socket.send(uid_tag.encode("utf-8"))
+ 
+client_socket.close()
---
+++ rfid/rfid_server.py
@@ -0,0 +1,37 @@
+import socket
+import MySQLdb             
+
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+host = '192.168.0.107' //Need to change this everytime
+port = 12002         
+
+server_socket.bind((host, port))        
+server_socket.listen(5)
+client, addr = server_socket.accept()
+
+while True:	     
+	uid_tag = client.recv(1024).decode("utf-8")
+	enc_tag = "".join("{:02x}".format(ord(c)) for c in uid_tag)
+
+	db = MySQLdb.connect(host = 'localhost', user = 'root', passwd = '1192001',db = 'project')
+	str = 'select name, gender, designation, city from rfid where id = ' + enc_tag
+	cur = db.cursor()
+	cond = cur.execute(str)
+
+	if cond!=0:
+		print("Database identified")
+		for row in cur.fetchall():
+			print(row)
+	
+	else:
+		print("Not A Genuine Person")
+	
+	db.close()
+
+client.close() 
+
+
+
+
+               
